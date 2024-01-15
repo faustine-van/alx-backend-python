25dac82b75f5a28caaf3a7e358bea040c57a2eec
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-15 12:02:57 +0200

    fixed annotated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,5 +7,5 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """return random delay between 0 and max_delay"""
-    random_float = random.uniform(0, max_delay)
+    random_float: float = random.uniform(0, max_delay)
     return random_float
