47396cee619418d6766bbb0f1fa4ce9c146b4d1e
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-16 12:11:37 +0200

    asynchronous comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ async def async_generator():
        - each time asynchronously wait 1 second,
        - yield a random number between 0 and 10.
     """
+    resOfFloat = random.uniform(0, 10)
     for i in range(10):
-        resOfFloat = random.uniform(0, 10)
         await asyncio.sleep(1)
         yield resOfFloat
