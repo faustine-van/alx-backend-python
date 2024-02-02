2a071d395d319ff9b59c1b3d2d19ebd75d546821
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-02 22:43:55 +0200

    parameterized and assertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -35,22 +35,24 @@ class TestGetJson(unittest.TestCase):
     """
     estGetJson.test_get_json
     """
-    @patch('utils.requests.get')
-    def test_get_json(self, mocked_get):
+    def test_get_json(self):
         """test_access_nested_map_exception"""
-        test_data = [
-          {"url": "http://example.com", "test_payload": {"payload": True}},
-          {"url": "http://holberton.io", "test_payload": {"payload": False}},
-        ]
-        for data in test_data:
-            test_url = data['url']
-            test_payload = data['test_payload']
+        with patch('utils.requests.get') as mocked_get:
+            test_data = [
+             {"url": "http://example.com",
+              "test_payload": {"payload": True}},
+             {"url": "http://holberton.io",
+              "test_payload": {"payload": False}},
+            ]
+            for data in test_data:
+                test_url = data['url']
+                test_payload = data['test_payload']
 
-            mock_res = Mock()
-            mock_res.json.return_value = test_payload
-            mocked_get.return_value = mock_res
+                mock_res = Mock()
+                mock_res.json.return_value = test_payload
+                mocked_get.return_value = mock_res
 
-            res = get_json(test_url)
-            mocked_get.assert_called_once_with(test_url)
-            self.assertEqual(res, test_payload)
-            mocked_get.reset_mock()
+                res = get_json(test_url)
+                mocked_get.assert_called_once_with(test_url)
+                self.assertEqual(res, test_payload)
+                mocked_get.reset_mock()
