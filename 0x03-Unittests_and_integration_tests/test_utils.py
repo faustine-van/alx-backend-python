e76eacb93435e08d706901edf275c768bcd9fa94
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-04 13:55:42 +0200

    fixed: type annotated

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -55,17 +55,13 @@ class TestGetJson(unittest.TestCase):
 class TestMemoize(unittest.TestCase):
     """test memoize"""
     def test_memoize(self):
-        """method"""
 
         class TestClass:
-            """TestClas"""
             def a_method(self):
-                """_method"""
                 return 42
 
             @memoize
             def a_property(self):
-                """a_property"""
                 return self.a_method()
         with patch.object(TestClass, 'a_method', return_value=42) as mok:
             instance = TestClass()
