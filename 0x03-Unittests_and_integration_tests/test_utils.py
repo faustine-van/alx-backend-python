2824beacadea284ae026d5a341fdfb2f123b13c2
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-06 08:41:07 +0200

    add required file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,8 +14,8 @@ class TestAccessNestedMap(unittest.TestCase):
     TestAccessNestedMap.test_access_nested_map
     """
     @parameterized.expand([
-        ({"a": 1}, ['a',], 1),
-        ({"a": {"b": 2}}, ['a',], {"b": 2}),
+        ({"a": 1}, ['a', ], 1),
+        ({"a": {"b": 2}}, ['a', ], {"b": 2}),
         ({"a": {"b": 2}}, ['a', 'b'], 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
@@ -23,7 +23,7 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ['a',]),
+        ({}, ['a', ]),
         ({"a": 1}, ['a', 'b'])
     ])
     def test_access_nested_map_exception(self, nested_map, path):
